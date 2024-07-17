2df8bc1b250ba1c44f21def41e3132269b4f7a44
TurtleP <jpostelnek@outlook.com>
2024-07-17 17:12:14 -0400

    update tasman quest

--- src/data/showcase.tsx
+++ src/data/showcase.tsx
@@ -10,7 +10,7 @@ const Showcase: Array<ShowcaseItem> = [
   {
     title: "Taśman Quest",
     author: "Nawias",
-    description: "Join Taśman in his quest to wrap everything in TAŚMAN Sealed duct tape! In this short, arcade-y game for your Nintendo 3DS explore the famous Lewiatan grocery store of Księżomierz and flex your tape-wrapping skills as the clock is ticking!",
+    description: "Join Taśman in his quest to wrap everything in TAŚMAN Sealed duct tape! Flex your tape-wrapping skills as the clock is ticking!",
     preview: require("/showcase/tasmanquest.png"),
     source: "https://nawias.itch.io/tasmanquest"
   }
